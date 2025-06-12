f1f78e1dd1bf8004f256776cc318b12d9a86cc52
lorebindi <lorenzo.bindi02@gmail.com>
2025-06-12 19:04:13 +0200

    Initial commit

---
+++ Parser.py
@@ -0,0 +1,126 @@
+import libsbml
+import re
+
+
+def readSBMLfile(filename):
+    reader = libsbml.SBMLReader()
+    SBMLdocument = reader.readSBML(filename)
+
+    # Check the validity of the document
+    if SBMLdocument.getNumErrors() > 0:
+        print("Reading error")
+        SBMLdocument.printErrors()
+        return None
+
+    # Make sure the file uses level 3
+    if SBMLdocument.getLevel() != 2:
+        print("The file isn't level 2.")
+        return None
+
+    # Error checking
+    # TODO eventualmente aggiungere nei parametri della funzione il campo "inConversion=True", per abilitare la
+    #  conversione se non compatibile
+    if SBMLdocument.checkL2v3Compatibility() > 0:
+        print("The file is not compatible with level 2 version 3")
+        return None
+
+    model = SBMLdocument.getModel()
+    if model is None:
+        print("Unable to create Model object.")
+        return None
+
+    return model
+
+def extract_species(model):
+    species = {}
+    for s in model.getListOfSpecies():
+        if s.isSetInitialAmount(): # We have the absolute initial amount.
+            species[s.getId()] = (s.getName(), s.getInitialAmount())
+        elif s.isSetInitialConcentration(): # We have to do: amount = volume * InitialConcetrantion.
+            compartment = model.getCompartment(s.getCompartment())
+            volume = compartment.getSize()
+            try:
+                species[s.getId()] = (s.getName(), volume * s.getInitialConcentration())
+            except TypeError as e: # Example: value * None
+                species[s.getId()] = (s.getName(), None)
+
+    return species
+
+def extract_reactions(model):
+    reactions = []
+
+    for r in model.getListOfReactions():
+        reaction = {
+            "id": r.getId(),
+            "reactants": {},
+            "products": {},
+            "rate": None,
+            "parameters": {}
+        }
+
+        # Reactans' coefficients
+        reactants_ids = []
+        for sr in r.getListOfReactants():
+            reactants_ids.append(sr.getId()) #usefull to check if the kinectik law is mass action law
+            reaction["reactants"][sr.getSpecies()] = sr.getStoichiometry()
+
+        # Products' coefficients
+        for sp in r.getListOfProducts():
+            reaction["products"][sp.getSpecies()] = sp.getStoichiometry()
+
+        # Kinectic law
+        kinetic_law = r.getKineticLaw()
+        if kinetic_law is None:
+            return None
+
+        formula = kinetic_law.getFormula()
+        if not is_mass_action(formula, reactants_ids):
+            return None
+
+        reaction["rate"] = formula
+
+        #TODO: supporto ad altre o qualsiasi legge cineticha
+
+        #TODO: inferimento della legge cinetica da dati forniti tramite file csv.
+        # 1. Come diciamo quale reazione deve passare da l'inferimento?
+        # 2. Generazione punti dati da cui inferire?
+
+        # Parametri
+        params = {p.getId(): p.getValue() for p in kinetic_law.getListOfParameters()}
+        reaction["parameters"] = params
+
+        reactions.append(reaction)
+
+    return reactions
+
+def is_mass_action(formula, reactant_ids):
+    if formula is None:
+        return False
+
+    # Rimuovi spazi
+    formula = formula.replace(" ", "")
+
+    # La formula deve contenere solo una costante e i reagenti (es. "k*A*B")
+    pattern = r'^[\w\.]+(\*[\w\.]+)*$'
+    if not re.fullmatch(pattern, formula):
+        return False
+
+    # Tutti i reagenti devono essere presenti
+    for rid in reactant_ids:
+        if rid not in formula:
+            return False
+
+    return True
+
+
+def gillespie_ssa (model, t_max):
+    t = 0
+    state = extract_species(model)
+    reactions = extract_reactions(model)
+
+
+
+
+
+
+    return None
--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 # This is a sample Python script.
-
+import Parser
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    model=Parser.readSBMLfile('/home/lorenzo/Desktop/ReacSim/Example/1.xml')
+    Parser.gillespie_ssa(model,10)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
