b179afc85366916e035dabc879bb480ba619b070
Elialeonardi02 <103329704+Elialeonardi02@users.noreply.github.com>
2025-06-13 14:39:06 +0200

    path file xml nel main

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # This is a sample Python script.
 import Parser
+import os
+
+
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -7,8 +10,10 @@ import Parser
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    model=Parser.readSBMLfile('/home/lorenzo/Desktop/ReacSim/Example/2.xml')
-    evolution=Parser.gillespie_ssa(model,10.0)
+    current_dir = os.path.dirname(__file__)
+    file_path = os.path.join(current_dir, "Example", "2.xml")
+    model = Parser.readSBMLfile(file_path)
+    evolution=Parser.gillespie_ssa(model,1)
     Parser.plot(evolution)
 
 
